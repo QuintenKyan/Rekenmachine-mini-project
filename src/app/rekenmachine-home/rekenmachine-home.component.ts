87d53c25192d58eea40d8772ec5ae56b72c93d20
QuintenKyan <152339@ettyhillesumlyceum.nl>
2021-11-19 15:41:46 +0100

    Initial commit

---
+++ src/app/rekenmachine-home/rekenmachine-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rekenmachine-home',
+  templateUrl: './rekenmachine-home.component.html',
+  styleUrls: ['./rekenmachine-home.component.css']
+})
+export class RekenmachineHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
